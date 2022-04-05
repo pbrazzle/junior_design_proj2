c129aaa1e2a3926a0b54d392dba60a278fa7f03f
pbrazzle <prb50@pitt.edu>
2022-04-05 13:58:53 -0400

    Added some speaker functionality

---
+++ BopItMain/BopItScreen.hpp
@@ -0,0 +1,40 @@
+#ifndef BOPITSCREEN
+#define BOPITSCREEN
+
+//Using Sunfounder LCD 1602 which works with the LiquidCrystal.h Arduino library
+#include <LiquidCrystal.h>
+
+namespace BopItScreen
+{
+	void gameWonDisplay()
+	{
+		
+	}
+	
+	void gameLostDisplay()
+	{
+		
+	}
+	
+	void gameStartDisplay()
+	{
+		
+	}
+	
+	void digItDisplay()
+	{
+		
+	}
+	
+	void popItDisplay()
+	{
+		
+	}
+
+	void shakeItDisplay()
+	{
+		
+	}	
+}
+
+#endif
---
+++ BopItMain/BopItSound.hpp
@@ -0,0 +1,80 @@
+#ifndef BOPITSOUND
+#define BOPITSOUND
+
+namespace BopItSound
+{	
+	struct note
+	{
+		double freq, time;
+	};
+	
+	namespace
+	{
+		const note A4 = {440, 0.2};
+		
+		const int gameOverLength = 1;
+		const note gameOverSong[gameOverLength] = {A4};
+	}
+	
+	void initializeSpeaker()
+	{
+		TCCR2A = 0x00; 								// Wave Form Generation Mode 0: Normal Mode; OC2A disconnected
+		TCCR2B = (1<<CS22) + (1<<CS21) + (1<<CS20); // prescaler = 1024
+		TIMSK2 = 0; 								// interrupt when TCNT2 is overflowed
+	}
+	
+	void playTone(const note n)
+	{
+		//Turn on speaker
+		TIMSK2 = (1<<TOIE2);	
+		
+		//Set frequency
+		
+		delay(n.time);
+		
+		//Turn off speaker
+		TIMSK2 = 0;				
+		digitalWrite(SPEAKER, 0);
+	}
+	
+	void gameOverMusic()
+	{
+		//for (int i = 0; i < gameOverLength; i++) playTone(gameOverSong[i]);
+	}
+	
+	void gameWonMusic()
+	{
+		
+	}
+	
+	void gameLostMusic()
+	{
+		
+	}
+	
+	void gameStartMusic()
+	{
+		
+	}
+	
+	void digItMusic()
+	{
+		
+	}
+	
+	void popItMusic()
+	{
+		
+	}
+	
+	void shakeItMusic()
+	{
+		
+	}
+}
+
+ISR(TIMER2_OVF_vect){
+    PORTD ^= (1<<SPEAKER); // toggle speaker pin
+}
+
+#endif
