1d0d83f7e043fc09424fa25149e4e14cba91a408
pbrazzle <prb50@pitt.edu>
2022-04-12 12:09:45 -0400

    Simpler speaker impl

--- BopItMain/BopItSound.hpp
+++ BopItMain/BopItSound.hpp
@@ -20,22 +20,23 @@ namespace BopItSound
 	
 	void initializeSpeaker()
 	{
+		/*
 		TCCR2A = 0x00; 								// Wave Form Generation Mode 0: Normal Mode; OC2A disconnected
 		TCCR2B = (1<<CS22) + (1<<CS21) + (1<<CS20); // prescaler = 1024
 		TIMSK2 = 0; 								// interrupt when TCNT2 is overflowed
+		*/
 	}
 	
 	void playTone(const note n)
 	{
-		//Turn on speaker
-		TIMSK2 = (1<<TOIE2);	
-		
-		//Set frequency
-		
-		delay(n.time);
-		
-		//Turn off speaker
-		TIMSK2 = 0;				
+		int startTime = millis();
+		while (millis() - startTime < n.time*1000)
+		{
+			digitalWrite(SPEAKER, 0);
+			delay(1000/n.freq);
+			digitalWrite(SPEAKER, 1);
+			delay(1000/n.freq);
+		}			
 		digitalWrite(SPEAKER, 0);
 	}
 	
@@ -74,9 +75,10 @@ namespace BopItSound
 		
 	}
 }
-
+/*
 ISR(TIMER2_OVF_vect){
     PORTD ^= (1<<SPEAKER); // toggle speaker pin
 }
+*/
 
 #endif
